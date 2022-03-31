89224d24e22972e438b3bd8f01b47801556d6e6b
Trevisan Martin Gabriel <aguerotrevisanmartin@gmail.com>
2022-03-31 18:24:53 -0300

    Modificaciones trabajando con aritmética de punteros para recorrer el vector

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -10,14 +10,15 @@ int main(){
 
     int i;
     double vt[N];
+    double* punt_vt = vt;
 
     for (i = 0; i < N; i++)
     {
-        vt[i] = 1+rand()%100;
-        printf("%.2f ",vt[i]);
-
+        *punt_vt = 1+rand()%100;
+        printf("%.2f ",*punt_vt);
+        punt_vt++;
     }
-    
+    // Trabajando con aritmetica de punteros
 
     return 0;
 
